87001ef017a75cc33a2433ffd37535e66f8e606b
Cem Çevik <tonbaliklimakarnha@gmail.com>
2019-04-22 01:10:49 +0200

    analytics eklendi

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -27,3 +27,11 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
   // ...
 });
+<!-- Global site tag (gtag.js) - Google Analytics -->
+< script > window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-61820447-13'); < /script>
